61889ae030d74921e5a8eba66d03d1da5a6defc7
EldorGroup <eldorgrup@gmail.com>
2025-12-23 21:24:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// הגדרה שהשרת יקרא את הקבצים מתוך תיקיית public
+// אומרים לשרת לחפש את האתר בתוך תיקיית public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// בכל פעם שמישהו נכנס לאתר, הוא יקבל את index.html המעוצב
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log('Eldor Group Server is running on port ' + port);
+  console.log('Server is live!');
 });
