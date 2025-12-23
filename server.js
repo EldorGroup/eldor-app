74066dc33ba92b6973e7362509820501e2126c10
EldorGroup <eldorgrup@gmail.com>
2025-12-23 21:26:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// אומרים לשרת לחפש את האתר בתוך תיקיית public
+// השורה הזו אומרת לשרת להגיש את כל הקבצים מתוך תיקיית public
 app.use(express.static(path.join(__dirname, 'public')));
 
+// השורה הזו היא התיקון - היא מכריחה את השרת לפתוח את ה-index.html כשנכנסים לכתובת הראשית
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// לכל מקרה של ניתוב אחר
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log('Server is live!');
+  console.log('Server is running on port ' + port);
 });
